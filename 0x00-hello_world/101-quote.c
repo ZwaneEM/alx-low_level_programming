8f8a0510bd8df5aabe879fcf15137f7cfe983053
ZwaneEM <zwaneem68@outlook.com>
2023-06-16 03:11:33 +0200

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - prints a string of text
+ * Return: Returns a 0
+ */
+int main(void)
+{
+        printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+
+        return (1);
+}
+
