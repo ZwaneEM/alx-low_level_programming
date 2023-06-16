f5e51eb9301bde9ad2009537a8b1165460703e65
ZwaneEM <zwaneem68@outlook.com>
2023-06-16 20:49:47 +0200

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -9,12 +9,12 @@ int main(void)
 	/*initialization*/
 	char alpha;
 
-	for (alpha = 'a'; alpha < 'z'; alpha++)
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
 	{
 		putchar(alpha);
 	}
 
-	for (alpha = 'A'; alpha < 'Z'; alpha++)
+	for (alpha = 'A'; alpha <= 'Z'; alpha++)
 	{
 		putchar(alpha);
 	}
