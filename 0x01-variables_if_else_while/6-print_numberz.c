21677871c88aa24e18f793899dd3772cbcdadff9
ZwaneEM <zwaneem68@outlook.com>
2023-06-16 21:56:08 +0200

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints a string of numbers
+ * Return: Returns a 0
+ */
+int main(void)
+{
+	char number;
+
+	for (number = '0'; number <= '9'; number++)
+	{
+		putchar(number);
+	}
+	putchar('\n');
+	return (0);
+}
+
