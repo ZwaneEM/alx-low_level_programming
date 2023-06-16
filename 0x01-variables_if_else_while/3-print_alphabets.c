e5a57507fd4b8b28f2a8b9fd2b0e25ff5bd6dcc0
ZwaneEM <zwaneem68@outlook.com>
2023-06-16 20:38:06 +0200

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - prints upper & lower case alphabets
+ * Return: Returns a 0
+ */
+int main(void)
+{
+	/*initialization*/
+	char alpha;
+
+	for (alpha = 'a'; alpha < 'z'; alpha++)
+	{
+		putchar(alpha);
+	}
+
+	for (alpha = 'A'; alpha < 'Z'; alpha++)
+	{
+		putchar(alpha);
+	}
+	return (0);
+}
+
