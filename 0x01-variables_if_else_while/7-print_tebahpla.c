55e121cc50ccaac1136503a8564fb0392ace2b19
ZwaneEM <zwaneem68@outlook.com>
2023-06-16 22:03:52 +0200

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - prints all alphabets on one
+ * line
+ * Return: returns a 0
+ */
+int main(void)
+{
+	/*initializing characters*/
+	char ch;
+
+	for (ch = 'z'; ch <= 'a'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+
+	return (0);
+}
