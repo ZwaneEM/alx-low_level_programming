6d4f122e4f7b030ca7aba810df072f49b392fe18
ZwaneEM <zwaneem68@outlook.com>
2023-06-16 02:04:47 +0200

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <conio.h>
 
 /**
  *main - prints the sizes of different data types
@@ -7,13 +6,18 @@
 */
 int main(void)
 {
-	clrscr();
-	printf("Size of a char: %2d byte(s) \n", sizeof(char));
-	printf("Size of a int:  %2d byte(s) \n", sizeof(int));
-	printf("Size of a long int: %2d byte(s) \n", sizeof(long int));
-	printf("Size of a long long int: %2d byte(s) \n", sizeof(long long int));
-	printf("Size of a float: %2d byte(s) \n", sizeof(float));
-	getch();
+	int intT;
+	float floatlT;
+	long int longT;
+	long long int longlT;
+	char charT;
+
+	/*sizeof evaluates the size of a variable*/
+	printf("Size of char: %zu byte(s)\n", sizeof(charT));
+	printf("Size of int: %zu byte(s)\n", sizeof(intT));
+	printf("Size of long int: %zu byte(s)\n", sizeof(longT));
+	printf("Size of long long int: %zu byte(s)\n", sizeof(longlT));
+	printf("Size of float: %zu byte(s)\n", sizeof(floatlT));
 
 	return (0);
 }
