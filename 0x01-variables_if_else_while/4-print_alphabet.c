fd1d166957a5bf9f63efa6ef4227a4330457a680
ZwaneEM <zwaneem68@outlook.com>
2023-06-16 21:19:49 +0200

    4-print_alphabet.c

---
+++ 0x01-variables_if_else_while/4-print_alphabet.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - prints all alphabets on one
+ * line
+ * Return: returns a 0
+ */
+int main(void)
+{
+	/*initializing characters*/
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		if (ch == e || ch == q)
+		{
+			continue;
+		}
+		else
+		{
+			putchar(ch);
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
+
