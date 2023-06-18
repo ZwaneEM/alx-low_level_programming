a27bcf2129f598c3b962636afd52780cd8fdcaf2
ZwaneEM <zwaneem68@outlook.com>
2023-06-18 02:12:26 +0200

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - prints a string of text
+ * Return: Returns a 0
+ */
+int main(void)
+{
+	int number = 0;
+	int snd = 0;
+	int rep = 9;
+	int i;
+	int n;
+	/*code goes here*/
+	for (n = 0; n < 9; n++)
+	{
+		for (i = 0 ; i < rep; i++)
+		{
+			putchar('0' + number);
+			putchar('1' + i + snd);
+				if (rep > 1)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+		}
+	rep--;
+	number++;
+	snd++;
+	}
+	return (0);
+}
