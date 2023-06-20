6a998a087650d1492d44a14d4cb77e6da7443335
ZwaneEM <zwaneem68@outlook.com>
2023-06-20 19:16:28 +0200

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - prints a string of text
+ * Return: Returns a 0
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('P');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+void _putchar(char t)
+{
+    putchar(t);
+}
