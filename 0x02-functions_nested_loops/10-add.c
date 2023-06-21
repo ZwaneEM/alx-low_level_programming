ac824dfab7f86ed1c3f82171a3bc8b000f0b2367
ZwaneEM <zwaneem68@outlook.com>
2023-06-21 03:01:33 +0200

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * add - prints a string of text
+ * Return: Returns n + m
+ * @n: describe the parameter
+ * @m: describe the parameter
+ */
+int add(int n, int m)
+{
+	return (n + m);
+}
