6737d1e1fb9bf2d7e314facbb60077aabbb2a868
ZwaneEM <zwaneem68@outlook.com>
2023-06-21 02:51:31 +0200

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * _islower - prints a string of text
+ * Return: Returns a 0
+ * @c: describe the parameter
+ */
+int _islower(int c)
+{
+	int x, y, z, dm, g;
+
+	for (x = 0; x <= 9; x++)
+	{
+		for (y = 0; y <= 9; y++)
+		{
+			z = x * y;
+			if (z > 9)
+			{
+				dm = z % 10;
+				g = (z - dm) / 10;
+				_putchar(44);
+				_putchar(32);
+				_putchar(g + '0');
+				_putchar(dm + '0');
+			}
+			else
+			{
+				if (y != 0)
+				{
+					_putchar(44);
+					_putchar(32);
+					_putchar(32);
+				}
+				_putchar(z + '0');
+			}
+		}
+		_putchar('\n');
+	}
+}
