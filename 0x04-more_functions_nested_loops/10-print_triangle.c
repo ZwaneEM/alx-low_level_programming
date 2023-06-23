7ac942a82fe52c24c4b1c59939c3c7bb263896ee
ZwaneEM <zwaneem68@outlook.com>
2023-06-23 03:18:30 +0200

    10-print_triangle.c

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_triangle - prints #
+ * Return: void
+ * @size : number of # to print
+ */
+void print_triangle(int size)
+{
+	int a = 1;
+	int star = size - 1;
+	int i;
+	int z;
+	int r;
+
+	for (i = 0; i < size; i++)
+	{
+		for (z = 0; z < size - a; z++)
+		{
+			_putchar(' ');
+		}
+		for (r = 0; r < size - star; r++)
+		{
+			_putchar('#');
+		}
+		a++;
+		star--;
+		_putchar('\n');
+	}
+}
