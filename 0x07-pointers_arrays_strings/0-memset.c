da083f967b140712ca23bbb8cb9e548133836bec
ZwaneEM <zwaneem68@outlook.com>
2023-07-04 02:02:32 +0200

    pointer

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+#include  "main"
+
+/**
+* _memset -   copy number bytes form adress "from" to adress "to"
+* @s: content is to be copied
+* @b: Source of data to be copied
+* @n: bytes of the memory
+* Return: s
+*/
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i = 0;
+
+	for (; i < n; i++)
+	{
+		*(s + i) = b;
+	}
+
+	return (s);
+}
