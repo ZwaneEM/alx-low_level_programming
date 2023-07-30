ce2af58a3963f42a97a4190b8992c6d61c17e402
ZwaneEM <Zwaneem68@outlook.com>
2023-07-30 21:08:09 +0200

    100-first.c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+
+/**
+* print_first - executes before the main function
+* @void: the function gets nothing
+*
+* Return: nothing
+*/
+void print_first(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
