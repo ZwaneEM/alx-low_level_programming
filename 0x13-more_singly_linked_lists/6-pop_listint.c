40a2cdd38516ee773a254ee537b652aa6bd00c10
ZwaneEM <Zwaneem68@outlook.com>
2023-07-31 22:13:29 +0200

    7-get_nodeint.c

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * pop_listint - deletes the head node of the list
+ * @head: start of the list
+ * Return: the head node's data / 0 if the list is empty
+ */
+int pop_listint(listint_t **head)
+{
+	listint_t *temp;
+	int data;
+
+	if (head == NULL)
+		return (0);
+
+	temp = (*head)->next;
+	data = (*head)->n;
+
+	free(*head);
+
+	*head = temp;
+
+	return (data);
+}
---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,34 @@
+#include "lists.h"
+
+/**
+ * get_nodeint_at_index - return nth node of the list
+ * @head: singly linked list
+ * @index: is the index of the node
+ *
+ * Return: return NULL if node does not exists / the index of node
+ */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	listint_t *current = NULL;
+	int node = index + 1;
+	int n;
+
+	if (head == NULL)
+		return (NULL);
+
+	for (n = 0; n <= node; n++)
+	{
+		if (current == NULL)
+			current = head;
+
+		if (n == node - 1)
+		{
+			if (current == NULL)
+				return (NULL);
+			break;
+		}
+		current = current->next;
+	}
+
+	return (current);
+}
