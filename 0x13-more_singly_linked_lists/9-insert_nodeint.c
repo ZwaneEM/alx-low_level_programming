6eaaf75fade54096660d3e28f5fc6673b4614fd0
ZwaneEM <Zwaneem68@outlook.com>
2023-08-01 21:03:43 +0200

    9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -18,6 +18,14 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	if (!head || !new)
 		return (NULL);
 
+	if (idx == 0)
+	{
+		new->n = n;
+		new->next = *head;
+		*head = new;
+		return (*head);
+	}
+
 	for (i = 0; i <= idx; i++)
 	{
 		if (current == NULL)
