7748f590f966eb3f03621498f6ecb70c4eaa540c
ZwaneEM <Zwaneem68@outlook.com>
2023-08-08 01:44:55 +0200

    file io

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,16 +1,34 @@
 #include "main.h"
 
 /**
- * create_file - function that creates a file
- * @filename: is the name of the file to create
- * @text_content: is a NULL terminated string to write 
- * to file
- *
+ * create_file - function that creates a file.
+ * @filename: variable pointer
+ * @text_content: content file
+ * Description: Create a function that creates a file.
  * Return: 1 on success, -1 on failure
  */
 int create_file(const char *filename, char *text_content)
 {
-	/* Declaration and initilization */
+	int count = 0;
+	int fp;
 
+	if (filename == NULL)
+		return (-1);
 
+	if (text_content == NULL)
+		text_content = "";
 
+	while (text_content[count] != '\0')
+	{
+		count++;
+	}
+
+	fp = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+
+	if (fp == -1)
+		return (-1);
+
+	write(fp, text_content, count);
+
+	return (1);
+}
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -2,6 +2,8 @@
 #define _MAIN_H_
 
 #include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <fcntl.h>
