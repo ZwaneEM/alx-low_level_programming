5375b4f7be13adaa63aea39509ce372e912877d5
ZwaneEM <Zwaneem68@outlook.com>
2023-08-08 01:01:40 +0200

    0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -19,12 +19,11 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (filename == NULL)
 		return (0);
 
-	fp = open(filename, O_RDONLY);
-
 	file = malloc(letters);
-
 	if (file == NULL)
 		return (0);
+
+	fp = open(filename, O_RDONLY);
 
 	if (fp == -1)
 	{
