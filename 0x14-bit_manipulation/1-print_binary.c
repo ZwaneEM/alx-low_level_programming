e578c072dcd23f9e8e5aefd2e8cc10d5123e8608
ZwaneEM <Zwaneem68@outlook.com>
2023-08-31 23:04:23 +0200

    1-print_binary.c

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -8,39 +8,16 @@
  */
 void print_binary(unsigned long int n)
 {
-	unsigned long int index = 1;
-	unsigned long int ans = 0;
+	if (n >> 0)
+	{
+		if (n >> 1)
+			print_binary(n >> 1);
+		_putchar((n & 1) + '0');
 
-	if (n == 0)
+	}
+	else
 	{
 		_putchar('0');
-		return;
-	}
-
-	while (index < n && n != 0)
-	{
-		index *= 2;
-
-		if (index > n)
-		{
-			index /= 2;
-			break;
-		}
-	}
-
-	while (index >= 1)
-	{
-		ans += index;
-
-		if (ans <= n)
-		{
-			_putchar('1');
-		}
-		else
-		{
-			_putchar('0');
-			ans -= index;
-		}
-		index /= 2;
 	}
 }
+
