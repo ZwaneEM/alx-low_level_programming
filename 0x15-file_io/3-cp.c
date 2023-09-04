a0bc4c4068982042cdbade8880db7a049b420874
ZwaneEM <Zwaneem68@outlook.com>
2023-09-04 19:51:07 +0200

    3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -58,9 +58,17 @@ int cpy_files(char *filename_f, char *filename_t)
 			exit(99);
 		}
 	}
-
-	close(file_to);
-	close(file_from);
+	
+	if (close(file_from) == -1)
+	{
+	    dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
+		exit(100);
+	}
+	if (close(file_to) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_to);
+		exit(100);
+	}
 
 	return (1);
 }
